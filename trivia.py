fd5a59f2f1adc8d1071386846bebf4fe93b40955
Nick <nickklann1@gmail.com>
2022-11-17 20:10:00 -0500

    Nick docstrings and new method

--- trivia.py
+++ trivia.py
@@ -13,18 +13,33 @@ class Player:
     """
     
     def __init__(self, name, score):
-        """
+        """ Initalizes the player object
+
+            Args:
+                name (String) - Name of the player
+                score (Int) - Current score of the player
+            Side Effects:
+                Sets all attributes for the Player class
         """
         
     def getScore(self):
-        """
+        """ Return the current score of the player
+
+            Returns:
+                Return the current highscore for the player
         """
     
-    def updateScore(self, path):
-        """
+    def updateScore(self):
+        """ Update the player score
+
+            Side Effect:
+                Compares current highscore to most recent score and updates
+                the highscore.
         """
     
-    def 
+    def resetScore(self, name):
+        """
+        """
 
 class Questions:
     """
