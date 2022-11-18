f36decd80ba6b391e7fdb44d43738db451b1bd74
DZou64 <112503640+DZou64@users.noreply.github.com>
2022-11-17 19:56:51 -0500

    Project layout and David's docstrings

---
+++ trivia.py
@@ -0,0 +1,49 @@
+"""Run a UMD trivia game with one player.
+"""
+import ArgumentParser
+import pandas
+import sys
+
+class Player:
+    """Creates a Player object.
+    
+        Attributes:
+            name (string) - Name of the player.
+            score (int) - Current score of the player.
+    """
+    
+    def __init__(self, name, score):
+        """
+        """
+        
+    def getScore(self):
+        """
+        """
+    
+    def updateScore(self, path):
+        """
+        """
+    
+    def 
+
+class Questions:
+    """
+    """
+    
+    def __init__(self, path):
+        """
+        """
+    
+    def selectQuestion(self):
+       """
+       """ 
+
+def main():  
+    """
+    """
+
+def parseargs(arglist):
+    """
+    """
+
+if __name__ == "__main__":
